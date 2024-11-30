e21426b299fe339dd24e328d1e1060d348fc6012
Kagol <kagol@sina.com>
2024-11-30 08:24:49 +0800

    init project

---
+++ web/src/views/menu/demo/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <Breadcrumb
+      :items="['menu.menuPage', 'menu.menuPage.second', 'menu.add.demo']"
+    />
+    <div class="content">
+      <div class="content-main">
+        <img src="@/assets/images/tiny-logo.png" class="image" />
+        <h3 class="tip">{{ $t('exception.result.demo.description') }}</h3>
+        test
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped lang="less">
+  @import '@/assets/style/exception.less'; /* 引入公共样式 */
+</style>
